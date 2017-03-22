bb840f01c6a013a140141a523f9811161068d141
Niles Rowland <neterslandreau@gmail.com>
2017-03-22 08:51:00 -0400

    some styling.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
 				<div class="intro-text">
 					<span class="name">The Neters Group</span>
 					<hr class="star-light">
-					<span class="skills">Software Engineer - Web Developer - Graphic Artist</span>
+					<span class="skills">Software Engineering - Web Application Development - Graphics Generation</span>
 				</div>
 			</div>
 		</div>
