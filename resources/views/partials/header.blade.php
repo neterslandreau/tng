242b4b2d0c3610aba2254bac6489adb8d716af47
Niles Rowland <neterslandreau@gmail.com>
2017-03-23 19:59:03 -0400

    work on the logo.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,11 +2,11 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				<img class="img-responsive" src="img/profile.png" alt="">
+				<img class="img-responsive" src="img/profile1.png" alt="">
 				<div class="intro-text">
 					<span class="name">The Neters Group</span>
 					<hr class="star-light">
-					<span class="skills">Software Engineering - Web Application Development - Web Hosting Services</span>
+					<span class="skills">Web Application Development - Web Hosting Services</span>
 				</div>
 			</div>
 		</div>
