c07c09bb7df4ef1ae535af3ca7e6ee576358f33c
Niles Rowland <neterslandreau@gmail.com>
2017-03-25 10:04:05 -0400

    fix remember me feature.

--- app/User.php
+++ app/User.php
@@ -7,24 +7,32 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-    use Uuids;
+	use Notifiable;
+	use Uuids;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'email', 'password', 'last_name', 'first_name', 'username', 'role',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'email', 'password', 'last_name', 'first_name', 'username', 'role',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
+	
+	/**
+	* Indicates if the ID's are auto-incrementing
+	*
+	* @var bool
+	*/
+	public $incrementing = false;
+
 }
