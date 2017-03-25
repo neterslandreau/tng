d40966f7cdb962c6b7b413ad9376abb8bead211f
Niles Rowland <neterslandreau@gmail.com>
2017-03-25 08:44:48 -0400

    some formatting for readability.

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -19,37 +19,12 @@
 
 			<hr class="star-primary">
 			<div class="col-md-8 col-md-offset-2">
-			<h3 class="text-center">Hosting Details</h3>
+
+				<h3 class="text-center">Hosting Details</h3>
 				<p>Servers are hosted by Digital Ocean and managed by Laravel Forge with Envoyer.</p>
 				<p>Image storage and SMTP services are provided by Amazon Web Services.</p>
-<!-- 
-				<div class="row">
-
-					<div class="col-md-5">
-
-					<h4>Hello</h4>
-					<ul>
-						<li>Mauris sollicitudin diam eget justo elementum aliquam.</li>
-						<li>Nulla tincidunt velit ac leo ultrices, vel pulvinar lectus posuere.</li>
-					</ul>
-
-					</div>
-
-					<div class="col-md-6 col-md-offset-1">
-
-					<h4>World</h4>
-					<ul>
-						<li>Mauris sollicitudin diam eget justo elementum aliquam.</li>
-						<li>Nulla tincidunt velit ac leo ultrices, vel pulvinar lectus posuere.</li>
-					</ul>
-
-					</div>
-
-				</div>
 
 			</div>
-
- -->		</div>
 
 	</div>
 </section>
