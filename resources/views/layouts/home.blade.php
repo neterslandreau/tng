07f0a8ea9040c942a345b37df776794bf1ca1214
Niles Rowland <neterslandreau@gmail.com>
2020-06-02 14:20:08 -0400

    add google analytics.

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -20,6 +20,16 @@
 			'csrfToken' => csrf_token(),
 		]) !!};
 	</script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-434138-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-434138-1');
+	</script>
 </head>
 <body id="">
 	<div id="app">
