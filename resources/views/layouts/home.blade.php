dc45b6615082ce192abc3771e284f7bfb3e0eccd
Niles Rowland <neterslandreau@gmail.com>
2020-06-02 14:26:28 -0400

    another ga setup.

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-434138-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-434138-1');
+	</script>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,15 +30,6 @@
 		]) !!};
 	</script>
 
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-434138-1"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'UA-434138-1');
-	</script>
 </head>
 <body id="">
 	<div id="app">
